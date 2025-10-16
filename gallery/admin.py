45998bb68b4c1a6c099ed3b2a8e5668d7bd51f33
SourTarte <dioncmcmillan@gmail.com>
2025-10-16 10:08:16 +0100

    Add media upload options to admin panel

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Media, Category
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Media
 
 # Register your models here.
-admin.site.register(Media)
-admin.site.register(Category)
+
+@admin.register(Media)
+class MediaAdmin(SummernoteModelAdmin):
+    """
+    Lists fields for display in admin, fileds for search,
+    field filters, fields to prepopulate and rich-text editor.
+    """
+    list_display = (
+        'title',
+        'category',
+        'created_on'
+    )
+    search_fields = ['title', 'subtitle', 'description',]
+    list_filter = ('category', 'created_on')
+    summernote_fields = ('description',)
